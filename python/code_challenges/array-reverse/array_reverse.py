61155c4c2508ef4a44e02d82f99849da103255d1
salsabilmislat <salsabilmislat196@gmail.com>
2021-10-31 20:40:25 +0200

    done

---
+++ python/code_challenges/array-reverse/array_reverse.py
@@ -0,0 +1,8 @@
+#newArray=list(input())
+
+def reverse(newArray):
+    # this function is taking array of number and reverse it
+    print(newArray[::-1])
+
+print("this is the Array after reversing :")
+reverse([1,2,3,4,5])
