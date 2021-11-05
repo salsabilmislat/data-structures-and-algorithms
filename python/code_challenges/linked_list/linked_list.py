ce26e1baf012d14df0ac3bf921d25a5c42c6bbda
salsabilmislat <salsabilmislat196@gmail.com>
2021-11-05 22:14:15 +0200

    new one

---
+++ python/code_challenges/linked_list/linked_list.py
@@ -0,0 +1,77 @@
+class Node:
+
+   def __init__(self,value):
+       self.value=value
+       self.next=None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head=None
+
+    def insert(self,value):
+
+        node = Node(value)
+
+        if self.head is None:
+            self.head = node
+
+        else:
+            current = self.head
+
+            while current.next != None:
+                current = current.next
+            current.next = node
+
+    def includes(self,value):
+
+        current = self.head
+
+        while current != None:
+            if current.value==value:
+                return True
+
+            current = current.next
+
+        return False
+
+    def __str__(self):
+
+        result = ""
+
+        if self.head is None:
+            result += None
+
+        else:
+            current = self.head
+
+            while(current):
+                result += "{ "+str(current.value)+" } -> "
+                current = current.next
+            result += "NULL"
+            return result
+
+
+
+if __name__=="__main__":
+
+    newlist=LinkedList()
+
+    newlist.insert(5)
+    newlist.insert(8)
+    newlist.insert(10)
+    newlist.insert(12)
+    newlist.insert(4)
+    print(newlist)
+
+    if newlist.includes(0):
+        print("hi")
+    else:
+        print("good luck")
+
+
+
+
+
+
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,76 @@
-from linked_list.linked_list import LinkedList
+from code_challenges.linked_list.linked_list import LinkedList,Node
+
+import pytest
 
 
-def test_import():
-    assert LinkedList
+
+def test_empty_linked_list():
+    newlist=LinkedList()
+    expected = None
+    actual = newlist.head
+    assert expected == actual
+
+def test_insert_linked_list():
+    newlist=LinkedList()
+    newlist.insert(10)
+    expected = "{ 10 } -> NULL"
+    actual = newlist.__str__()
+    assert expected == actual
+
+def test_first_node ():
+    node = Node(10)
+    expected = 10
+    actual = node.value
+    assert actual == expected
+
+def test_insert_multi_values():
+    newlist = LinkedList()
+    newlist.insert(10)
+    newlist.insert("a")
+    expected= "{ 10 } -> { a } -> NULL"
+    actual= newlist.__str__()
+    assert expected == actual
+
+
+def test_includes_linked_list(newlist):
+
+    expected=True
+    actual=newlist.includes(10)
+    assert actual==expected
+
+def test_not_includes_linked_list(newlist):
+
+    expected=False
+    actual=newlist.includes(5)
+    assert actual==expected
+
+def test_to_string_collection_values(newlist):
+
+    expected= "{ 10 } -> { a } -> { 4 } -> NULL"
+    actual= newlist.__str__()
+
+    assert expected==actual
+
+# def test_first_node(newlist):
+#     newlist=Node()
+#     node = newlist.head
+#     expected=10
+#     actual = node.value
+#     assert expected == actual
+#     # assert newlist.head.next.value == 1
+
+
+
+@pytest.fixture
+def newlist():
+    newlist = LinkedList()
+    newlist.insert(10)
+    newlist.insert("a")
+    newlist.insert(4)
+    return newlist
+
+
+
+
+
+
