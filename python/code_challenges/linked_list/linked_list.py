325ec55256dacde44d948fe0ad1b28b1e23d65ea
salsabilmislat <salsabilmislat196@gmail.com>
2021-11-07 23:14:01 +0200

    new things

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -10,20 +10,6 @@ class LinkedList:
     def __init__(self):
         self.head=None
 
-    def append(self,value):
-
-        node = Node(value)
-
-        if self.head is None:
-            self.head = node
-
-        else:
-            current = self.head
-
-            while current.next != None:
-                current = current.next
-            current.next = node
-
 
     def insert(self,value):
         node = Node(value)
@@ -58,19 +44,78 @@ class LinkedList:
             result += "NULL"
             return result
 
-    
+    ## the start of Lab 6
+
+
+    def append(self,value):
+
+        node = Node(value)
+
+        if self.head is None:
+            self.head = node
+
+        else:
+            current = self.head
+
+            while current.next != None:
+                current = current.next
+            current.next = node
+
+
+    def insertBefore(self,perValue,valuetoadd):
+        newnode=Node(valuetoadd)
+        current = self.head
+        # if current.value==None:
+        #     self.head=newnode
+
+        if self.head.value==perValue:
+            newnode.next=self.head
+            self.head=newnode
+
+
+        while current.next != None:
+            if current.next.value==perValue:
+             newnode=Node(valuetoadd)
+             newnode.next=current.next
+             current.next=newnode
+             return True
+
+            current = current.next
+        return False
+
+    def insertAfter(self,nextValue,valuetoadd):
+        newnode=Node(valuetoadd)
+        current = self.head
+        if current==None:
+            self.head=newnode
+
+        while current.value != None:
+            if current.value==nextValue:
+             newnode=Node(valuetoadd)
+             newnode.next=current.next
+             current.next=newnode
+             return True
+
+            if current.value==nextValue and current.next==None:
+                current.next=newnode.value
+                newnode.next=None
+
+
+            current = current.next
+        return False
 
 
 
 if __name__=="__main__":
 
     newlist=LinkedList()
-
+    newlist.insert(4)
     newlist.append(5)
     newlist.append(8)
     newlist.append(10)
-    newlist.append(12)
-    newlist.insert(4)
+
+    newlist.insertBefore(8,6)
+    newlist.insertAfter(10,20)
     print(newlist)
 
     if newlist.includes(0):
@@ -78,6 +123,25 @@ if __name__=="__main__":
     else:
         print("good luck")
 
+
+
+ # found=False
+        # if value.next == None:
+        #     return False
+
+        # else:
+        #     newNode=Node(valuetoadd)
+        #     newNode.next=value.next
+        #     value.next=newNode
+        # while current:
+        #     if current.value==valuetoadd:
+        #         prev.next==node
+        #         node.next=current
+        #         found= True
+        #         break
+
+        #     prev=current
+        #     current = current.next
 
 
 
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -3,7 +3,6 @@ from code_challenges.linked_list.linked_list import LinkedList,Node
 import pytest
 
 
-
 def test_empty_linked_list():
     newlist=LinkedList()
     expected = None
@@ -52,13 +51,48 @@ def test_to_string_collection_values(newlist):
     assert expected==actual
 
 
-# def test_first_node(newlist):
-#     newlist=Node()
-#     node = newlist.head
-#     expected=10
-#     actual = node.value
-#     assert expected == actual
-#     # assert newlist.head.next.value == 1
+## the start of Lab 6
+
+
+def test_Add_To_End(newlist):
+
+    newlist.append(15)
+    expected = "{ 4 } -> { a } -> { 10 } -> { 15 } -> NULL"
+    actual = newlist.__str__()
+    assert expected == actual
+
+def test_multi_values_to_end(newlist):
+    newlist.append(15)
+    newlist.append(20)
+    expected = "{ 4 } -> { a } -> { 10 } -> { 15 } -> { 20 } -> NULL"
+    actual = newlist.__str__()
+    assert expected == actual
+
+def test_insert_a_node_before(newlist):
+    newlist.insertBefore("a",5)
+    expected = "{ 4 } -> { 5 } -> { a } -> { 10 } -> NULL"
+    actual = newlist.__str__()
+    assert expected == actual
+
+
+def test_insert_a_node_after(newlist):
+    newlist.insertAfter("a",5)
+    expected = "{ 4 } -> { a } -> { 5 } -> { 10 } -> NULL"
+    actual = newlist.__str__()
+    assert expected == actual
+
+def test_node_before_the_first_node(newlist):
+
+    newlist.insertBefore(4,5)
+    expected = "{ 5 } -> { 4 } -> { a } -> { 10 } -> NULL"
+    actual = newlist.__str__()
+    assert expected == actual
+
+def test_after_the_last_node(newlist):
+    newlist.insertAfter(10,20)
+    expected = "{ 4 } -> { a } -> { 10 } -> { 20 } -> NULL"
+    actual = newlist.__str__()
+    assert expected == actual
 
 
 
